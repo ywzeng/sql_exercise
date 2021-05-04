a5ffad85899173e7873c4409e6248ccca7ea5276
ywzeng <ieywzeng@hotmail.com>
2021-05-05 00:58:30 +0800

    第N高的薪水

---
+++ leetcode_0177.sql
@@ -0,0 +1,15 @@
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+  SET N = N - 1;
+  RETURN (
+      # Write your MySQL query statement below.
+      SELECT IFNULL(
+          (
+            SELECT DISTINCT Salary 
+            FROM Employee
+            ORDER BY Salary DESC
+            LIMIT N, 1
+          ), NULL
+      ) AS getNthHighestSalary
+  );
+END
