045ec6774f47d95a454efd814aed64f307b8784a
ywzeng <ieywzeng@hotmail.com>
2021-05-05 00:33:51 +0800

    删除重复的电子邮箱

---
+++ leetcode_0196.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+delete from Person
+where Id not in (
+    select *
+    from (
+        select min(Id)
+        from Person
+        group by Email
+    ) as temp
+);
