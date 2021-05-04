1ae6eb7b3bdaf9802c8f2d88c365d6b0ee0b3b2f
ywzeng <ieywzeng@hotmail.com>
2021-05-04 23:42:59 +0800

    从不订购的客户

--- leetcode_0183.sql
+++ leetcode_0183.sql
@@ -1,8 +1,6 @@
 # Write your MySQL query statement below
-select Name as `Customers` from (
-    select c.Id as Id, c.Name as Name, o.Id as oid
-    from Customers as c
-    left join Orders as o
-    on c.Id = o.CustomerId
-) as temp 
-where oid is Null
+select c.Name as Customers
+from Customers as c
+left join Orders as o
+on c.Id = o.CustomerId
+where o.Id is Null
