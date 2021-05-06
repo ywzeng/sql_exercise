74ccc53381169a2c4ce3e4b2771cc2036b7afca0
ywzeng <ieywzeng@hotmail.com>
2021-05-06 19:03:15 +0800

    超过5名学生的课

--- leetcode_0596.sql
+++ leetcode_0596.sql
@@ -1,5 +1,12 @@
 # Write your MySQL query statement below
 select class
+from courses
+group by class
+having count(distinct student) >= 5
+
+
+# Write your MySQL query statement below
+select class
 from (
     select class, count(distinct student) as stu_num
     from courses
